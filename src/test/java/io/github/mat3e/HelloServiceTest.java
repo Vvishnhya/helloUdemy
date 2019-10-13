3cf5a799955b858993d80d97aab564caf9fe389f
Von_C <Von.Combinersky@gmail.com>
2019-10-13 23:56:04 +0200

    DB set up. managing Flyway,H2 and Hibernate embedded and connected. First flyway migrations. HelloService adjusted due to HibernateUtil

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -76,7 +76,7 @@ public class HelloServiceTest {
     private LangRepository FALLBACK_LANG_non_exisiting_IdRepository() {
         return new LangRepository() {
             @Override
-            Optional<Lang> findById(Long id) {
+            Optional<Lang> findById(Integer id) {
                 return Optional.empty();
             }
         };
@@ -85,7 +85,7 @@ public class HelloServiceTest {
     private LangRepository FALLBACK_LANG_IdRepository() {
         return new LangRepository() {
             @Override
-            Optional<Lang> findById(Long id) {
+            Optional<Lang> findById(Integer id) {
                 if (id.equals(HelloService.FALLBACK_LANG.getId())) {
                     return Optional.of(new Lang(null, FALLBACK_Id_WELCOME, null));
                 }
@@ -97,7 +97,7 @@ public class HelloServiceTest {
     private LangRepository alwaysReturningHelloRepository() {
         return new LangRepository() {
             @Override
-            Optional<Lang> findById(Long id) {
+            Optional<Lang> findById(Integer id) {
                 return Optional.of(new Lang(null, WELCOME, null));
             }
         };
