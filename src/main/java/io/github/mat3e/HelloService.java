7884500d946a5c8b01a79899ea37022db2471e5b
Von_C <Von.Combinersky@gmail.com>
2019-10-13 10:59:29 +0200

    Data: LanRepository added and HelloService adjuste to repo

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -5,11 +5,26 @@ import java.util.Optional;
 class HelloService {
 
     static final String FALLBACK_NAME = "world";
+    static final Lang FALLBACK_LANG = new Lang(1L, "Hello", "en");
+    private LangRepository repository;
+
+    HelloService(){
+        this(new LangRepository());
+    }
+
+    public HelloService(LangRepository repository) {
+        this.repository = repository;
+    }
 
     String prepareGreeting(String name){
-        // wycięte z servleta
-        // Optional.ofNullable(req.getParameter(NAME_PARAM)).orElse("world");
-        // i przerobione
-        return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + "!";
+         return prepareGreeting(name, FALLBACK_LANG.getId());
     }
+
+    String prepareGreeting(String name, Long langId) {
+        var welcomeMsg = repository.findById(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
+        var nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
+        return welcomeMsg + " " + nameToWelcome + "!";
+    }
+
+
 }
